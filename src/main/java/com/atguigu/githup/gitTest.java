15915bf57aeff33594731628bf4960e4e3d71b4f
linjinshan013 <93266616+linjinshan013@users.noreply.github.com>
2021-10-29 11:41:35 +0800

    Update gitTest.java

--- src/main/java/com/atguigu/githup/gitTest.java
+++ src/main/java/com/atguigu/githup/gitTest.java
@@ -8,5 +8,6 @@ public class gitTest {
         System.out.println("third test");
         System.out.println("hot_fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
