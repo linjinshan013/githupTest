5795f777ca1365c9d2db3ac0b570f939711b1362
ljs <ljs19983916@163.com>
2021-10-29 11:04:57 +0800

    hot_fix commit

--- src/main/java/com/atguigu/githup/gitTest.java
+++ src/main/java/com/atguigu/githup/gitTest.java
@@ -4,5 +4,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("git test");
         System.out.println("second test");
+        System.out.println("hot-fix test");
     }
 }
