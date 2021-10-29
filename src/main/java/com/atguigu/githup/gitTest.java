dc61cf5406b997e77add197e401b6665beacadd3
ljs <ljs19983916@163.com>
2021-10-29 11:18:29 +0800

    push test

--- src/main/java/com/atguigu/githup/gitTest.java
+++ src/main/java/com/atguigu/githup/gitTest.java
@@ -7,5 +7,6 @@ public class gitTest {
         System.out.println("hot-fix test");
         System.out.println("third test");
         System.out.println("hot_fix test");
+        System.out.println("push test");
     }
 }
